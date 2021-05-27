6376686f18e67fae4ce39742b1afbb638da057c3
Felix Altenburg <felix.n.altenburg@gmail.com>
2021-05-27 18:00:55 +0100

    added most of back end of movie list app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+require 'json'
+require 'open-uri'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+top_rated_movies = URI.open(url).read
+movies = JSON.parse(top_rated_movies)
+
+movie_images = movies['results']
+movie_images.each do |movie|
+  puts 'creating movie'
+  Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+  puts 'completed'
+end
